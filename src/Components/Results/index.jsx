33be5d93c0fbef4a7a354ca17d712394dba5471e
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-06-23 17:07:24 -0700

    lab29

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -4,11 +4,11 @@ const JSONPrettyTheme = require('react-json-pretty/dist/adventure_time');
 
 function Results(props){
   return (
-    <section data-testid="results-section">
+    <section>
       {
         props.loading
         ? <div>Waiting for search query OR loading...!</div>
-        : <JSONPretty id="json-pretty" theme={JSONPrettyTheme} data={props.data}></JSONPretty>
+        : <pre data-testid="results-section"><JSONPretty id="json-pretty" theme={JSONPrettyTheme} data={props.data}></JSONPretty></pre>
       }
     </section>
   );
